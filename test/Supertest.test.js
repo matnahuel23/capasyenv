11921c43f2b8145cf0ae23b4c5ab8c23afe6a610
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-12-10 20:43:07 -0300

    test ok

--- test/Supertest.test.js
+++ test/Supertest.test.js
@@ -47,5 +47,32 @@ describe("Testing SuperTest", () => {
                 throw error;
             }
         });
+
+        it("Status 400 cuando no existe la propiedad TITLE", async () => {
+            const noTitle = {
+                //"title": "Producto Prueba",
+                "description": "Producto de Prueba",
+                "code": 1,
+                "price": 10,
+                "status": true,
+                "stock": 100,
+                "category": "gaseosa"
+            };
+        
+            try {
+                const { statusCode, _body } = await requester
+                    .post('/products')
+                    .set('Cookie', [`connect.sid=${sessionCookie}`])
+                    .send(noTitle);
+        
+                expect(statusCode).to.equal(400);
+        
+                // Solo verificar que el statusCode sea 400, sin verificar el contenido exacto del mensaje de error
+            } catch (error) {
+                console.error("Error during test:", error);
+                throw error;
+            }
+        });
+                    
     });
 });

76e3d46037502c0cc94585e238b9f993bb86b4ee
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-12-16 08:40:13 -0300

    modifique modelo user con un arreglo de documentos y last_connection

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -1,18 +1,25 @@
-const mongoose = require ("mongoose");
-const paginate = require ("mongoose-paginate-v2")
+const mongoose = require("mongoose");
+const paginate = require("mongoose-paginate-v2");
+
+const documentSchema = new mongoose.Schema({
+  name: { type: String },
+  reference: { type: String }
+}, { _id: false });
 
 const userSchema = new mongoose.Schema({
-    first_name:{type: String},
-    last_name: {type: String},
-    email: {type: String},
-    age: {type: Number},
-    role:{ type: String, enum: ["user", "admin", "premium"], default: "user"},
-    password: { type: String }, 
-    cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Carrito' }
-},{ versionKey: false });
+  first_name: { type: String },
+  last_name: { type: String },
+  email: { type: String },
+  age: { type: Number },
+  role: { type: String, enum: ["user", "admin", "premium"], default: "user" },
+  password: { type: String },
+  cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Carrito' },
+  documents: { type: [documentSchema], default: [] }, // arreglo de documentos
+  last_connection: { type: Date }
+}, { versionKey: false });
 
 userSchema.plugin(paginate);
 
 const userModel = mongoose.model('Usuario', userSchema);
 
-module.exports = userModel
+module.exports = userModel;
